5e704196ed34ed14f1bf8ce0c57af1f6f2609a8f
k4ioken <kmahajan29.km@gmail.com>
2026-01-11 19:06:01 +0530

    added keyframes and fade-in classes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -9,9 +9,6 @@ import { StrategicAdvisoryBoardSection } from "../components/StrategicAdvisoryBo
 import Header from "../components/Header";
 
 const Home = () => {
-  // #region agent log
-  fetch('http://127.0.0.1:7242/ingest/09b30ba1-0da3-4167-af4a-2c2b8efe817c',{method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify({location:'Home.jsx:12',message:'Home component rendering',data:{},timestamp:Date.now(),sessionId:'debug-session',runId:'run1',hypothesisId:'B'})}).catch(()=>{});
-  // #endregion
   return (
     <div className="bg-[#f2f2f2] w-full relative">
       <Header />
