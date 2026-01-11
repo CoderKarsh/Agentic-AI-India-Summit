28a375766a24800c7e29bdfae62df75879e1e9ef
k4ioken <kmahajan29.km@gmail.com>
2026-01-11 23:58:44 +0530

    updated photo paths in speakerData

--- frontend/src/data/speakersData.js
+++ frontend/src/data/speakersData.js
@@ -1,236 +1,275 @@
+import Panneerselvam from "../assets/speakerPhotos/Panneerselvam.jpeg";
+import abhisheklal from "../assets/speakerPhotos/abishek lal.jpeg"
+import amangulati from "../assets/speakerPhotos/aman gulati.jpeg"
+import amitpradhan from "../assets/speakerPhotos/amit pradhan.jpeg"
+import anantshanmugam from "../assets/speakerPhotos/anant shanmugam.jpeg"
+import anilsingh from "../assets/speakerPhotos/anil singh.jpeg"
+import arpitsharma from "../assets/speakerPhotos/arpit sharma.jpeg"
+import ashishmishra from "../assets/speakerPhotos/ashish mishra.jpeg"
+import brajesh from "../assets/speakerPhotos/brajesh.jpeg"
+import charanjit from "../assets/speakerPhotos/charanjit.jpeg"
+import debashish from "../assets/speakerPhotos/debashish.jpeg"
+import dhirajagg from "../assets/speakerPhotos/dheeraj agg.jpeg"
+import geetha from "../assets/speakerPhotos/geetha.jpeg"
+import gurdeep from "../assets/speakerPhotos/gurdeep juneja.jpeg"
+import joginder from "../assets/speakerPhotos/joginder.jpeg"
+import lalitbaid from "../assets/speakerPhotos/lalit baid.jpeg"
+import narayanpani from "../assets/speakerPhotos/narayan panigrahi.jpeg"
+import nitinsatankar from "../assets/speakerPhotos/nitin satankar.jpeg"
+import pankajrai from "../assets/speakerPhotos/pankaj rai.jpeg"
+import prakashsah from "../assets/speakerPhotos/prakash sah.jpeg"
+import rajangupta from "../assets/speakerPhotos/rajan gupta.jpeg"
+import rajivchawla from "../assets/speakerPhotos/rajeev chawla.jpeg"
+import rajneshsingh from "../assets/speakerPhotos/rajnesh singh.jpeg"
+import rakeshrai from "../assets/speakerPhotos/Rakesh Rai.jpeg"
+import rishimohan from "../assets/speakerPhotos/rishi mohan.jpeg"
+import rishikesh from "../assets/speakerPhotos/rishikesh p.jpeg"
+import sandeep from "../assets/speakerPhotos/sandeep rajoriya.jpeg"
+import sanjaykhare from "../assets/speakerPhotos/sanjay khare.jpeg"
+import sanjeevkeshav from "../assets/speakerPhotos/sanjeev keshav.jpeg"
+import sharadnigam from "../assets/speakerPhotos/sharad nigam.jpeg"
+import shobhitmathur from "../assets/speakerPhotos/shobhit mathur.jpeg"
+import smsudershan from "../assets/speakerPhotos/sm sudershan.jpeg"
+import srinivas from "../assets/speakerPhotos/srinivas-d.jpeg"
+import sunildahiya from "../assets/speakerPhotos/sunil dahiya.jpeg"
+import sushillohia from "../assets/speakerPhotos/sushil lohia.jpeg"
+import vijaykalra from "../assets/speakerPhotos/vijay kalra.jpeg"
+import vinamramishra from "../assets/speakerPhotos/vinamra mishra.jpeg"
+import viveksaha from "../assets/speakerPhotos/vivek saha.jpeg"
+import yaduyadav from "../assets/speakerPhotos/yadu yadav.jpeg"
 export const speakersData = [
     {
       name: "Dr. Panneerselvam Madanagopal",
       title: "CEO",
       organization: "MeitY Startup Hub, GOI",
-      image: "https://c.animaapp.com/mk8nxs37jXzmJf/img/rectangle-331.png",
+      image: Panneerselvam
     },
     {
       name: "Sh. Vinamra Mishra",
       title: "Director",
       organization: "Ministry of Micro, Small and Medium Enterprises",
-      image: "https://c.animaapp.com/mk8nxs37jXzmJf/img/rectangle-331-11.png",
+      image: vinamramishra
     },
     {
       name: "Rakesh K. Rai",
       title: "Additional Development Commissioner & Joint Secretary",
       organization: "Ministry of MSME Government of India",
-      image: "https://c.animaapp.com/mk8nxs37jXzmJf/img/rectangle-331-2.png",
+      image: rakeshrai
     },
     {
       name: "Pankaj Rai",
       title: "Group Chief Data and Analytics Officer",
       organization: "Aditya Birla Group",
-      image: "https://c.animaapp.com/mk8nxs37jXzmJf/img/rectangle-331-3.png",
+      image: pankajrai
     },
     {
       name: "Ananthanarayanan Shanmugam",
       title: "Senior Vice President",
       organization: "Invest India",
-      image: "https://c.animaapp.com/mk8nxs37jXzmJf/img/rectangle-331-11.png",
+      image: anantshanmugam
     },
     {
       name: "Rishikesh Patankar",
       title: "Vice President",
       organization: "National Skill Development Corporation",
-      image: "https://c.animaapp.com/mk8nxs37jXzmJf/img/rectangle-331-11.png",
+      image: rishikesh
     },
     {
       name: "Dr. Sharad Nigam",
       title: "Group Senior Vice President Growth Strategist",
       organization: "Hitachi",
-      image: "https://c.animaapp.com/mk8nxs37jXzmJf/img/rectangle-331-11.png",
+      image: sharadnigam
     },
     {
       name: "Shobhit Mathur",
       title: "Partner",
       organization: "Ernst & Young",
-      image: "https://c.animaapp.com/mk8nxs37jXzmJf/img/rectangle-331-11.png",
+      image: shobhitmathur
     },
     {
       name: "Rajnesh Singh",
       title: "Chief Vigilance Officer",
       organization: "GAIL (India) Limited",
-      image: "https://c.animaapp.com/mk8nxs37jXzmJf/img/rectangle-331-11.png",
+      image: rajneshsingh
     },
     {
       name: "Rajiv K. Chawla",
       title: "Chairman",
       organization: "Integrated Association of Micro Small & Medium Enterprises of India",
-      image: "https://c.animaapp.com/mk8nxs37jXzmJf/img/rectangle-331-11.png",
+      image: rajivchawla
     },
     {
       name: "Nitin Kumar Satankar",
       title: "General Manager",
       organization: "Jio Platforms Limited",
-      image: "https://c.animaapp.com/mk8nxs37jXzmJf/img/rectangle-331-11.png",
+      image: nitinsatankar
     },
     {
       name: "Sanjeev Keshav",
       title: "Chief Executive Officer",
       organization: "Arvind Consultancy",
-      image: "https://c.animaapp.com/mk8nxs37jXzmJf/img/rectangle-331-11.png",
+      image: sanjeevkeshav
     },
     {
         name: "Lalit L Baid",
         title: "Co Founder",
         organization: "Tsuyo Manufacturing Pvt Ltd",
-        image: "https://c.animaapp.com/mk8nxs37jXzmJf/img/rectangle-331-11.png",
+        image:lalitbaid
       },
       {
         name: "Joginder Tanikella",
         title: "CEO",
         organization: "T-Works,Govt. of Telengana",
-        image: "https://c.animaapp.com/mk8nxs37jXzmJf/img/rectangle-331-11.png",
+        image: joginder
       },
       {
         name: "Ashish Mishra",
         title: "Director,Head of Vehicle Engineering",
         organization: "Ather Energy",
-        image: "https://c.animaapp.com/mk8nxs37jXzmJf/img/rectangle-331-11.png",
+        image: ashishmishra
       },
       {
         name: "Prakash Sah",
         title: "Managing Partner & Head,Data,Analytics & AI Manufacturing",
         organization: "TCS",
-        image: "https://c.animaapp.com/mk8nxs37jXzmJf/img/rectangle-331-11.png",
+        image: prakashsah
       },
       {
         name: "Amit Pradhan",
         title: "Vice President,IT & CIO",
         organization: "Dixon Technologies India Limited",
-        image: "https://c.animaapp.com/mk8nxs37jXzmJf/img/rectangle-331-11.png",
+        image: amitpradhan
       },
       {
         name: "Abhishek Lal",
         title: "CDO",
         organization: "Marks and Spencer Reliance India Private Limited",
-        image: "https://c.animaapp.com/mk8nxs37jXzmJf/img/rectangle-331-11.png",
+        image: abhisheklal
       },
       {
         name: "Geetha Adinarayan",
         title: "Distinguished Engineer,CTO",
         organization: "IBM Consulting,India and South Asia",
-        image: "https://c.animaapp.com/mk8nxs37jXzmJf/img/rectangle-331-11.png",
+        image: geetha
       },
       {
         name: "Dr. Narayan Panigrahi",
         title: "Associate Director Center of AI and Robotics",
         organization: "DRDO",
-        image: "https://c.animaapp.com/mk8nxs37jXzmJf/img/rectangle-331-11.png",
+        image: narayanpani
       },
       {
         name: "Debashish Roy",
         title: "Chief Digital Transformation Officer",
         organization: "CEAT Limited",
-        image: "https://c.animaapp.com/mk8nxs37jXzmJf/img/rectangle-331-11.png",
+        image: debashish
       },
       {
         name: "Anil Singh",
         title: "Senior Director",
         organization: "Flipkart",
-        image: "https://c.animaapp.com/mk8nxs37jXzmJf/img/rectangle-331-11.png",
+        image: anilsingh
       },
       {
         name: "Sushil Lohia",
         title: "Director Supply Chain & Procurement",
         organization: "KPMG India",
-        image: "https://c.animaapp.com/mk8nxs37jXzmJf/img/rectangle-331-11.png",
+        image: sushillohia
       },
       {
         name: "Charanjit Singh Bhatia",
         title: "General Manager Information Security",
         organization: "Marks and Spencer Reliance India Pvt Ltd",
-        image: "https://c.animaapp.com/mk8nxs37jXzmJf/img/rectangle-331-11.png",
+        image: charanjit
       },
       {
         name: "Srinivasan Dwarakanath",
         title: "Director General",
         organization: "Aerospace India Association",
-        image: "https://c.animaapp.com/mk8nxs37jXzmJf/img/rectangle-331-11.png",
+        image: srinivas
       },
       {
         name: "Sanjay Khare",
         title: "Professor of Practice",
         organization: "IIT Kanpur",
-        image: "https://c.animaapp.com/mk8nxs37jXzmJf/img/rectangle-331-11.png",
+        image: sanjaykhare
       },
       {
         name: "Vijay Kalra",
         title: "Consultant & Advisor",
         organization: "Execution Excellence",
-        image: "https://c.animaapp.com/mk8nxs37jXzmJf/img/rectangle-331-11.png",
+        image: vijaykalra
       },
       {
         name: "S.M. Sundaresan",
         title: "Senior Vice President",
         organization: "Havells India Ltd",
-        image: "https://c.animaapp.com/mk8nxs37jXzmJf/img/rectangle-331-11.png",
+        image: smsudershan
       },
       {
         name: "Vivek Saha",
         title: "Strategic Growth Partner",
         organization: "T-Works",
-        image: "https://c.animaapp.com/mk8nxs37jXzmJf/img/rectangle-331-11.png",
+        image: viveksaha
       },
       {
         name: "Sunil Dahiya",
         title: "Executive Vice President",
         organization: "Wadhwani Foundation",
-        image: "https://c.animaapp.com/mk8nxs37jXzmJf/img/rectangle-331-11.png",
+        image: sunildahiya
       },
       {
         name: "Gurdeep Singh Juneja",
         title: "Managing Director",
         organization: "Webdyn India Pvt. Ltd.",
-        image: "https://c.animaapp.com/mk8nxs37jXzmJf/img/rectangle-331-11.png",
+        image: gurdeep
       },
       {
         name: "Brajesh Kumar",
         title: "CEO",
         organization: "IASC Sector Skill Council",
-        image: "https://c.animaapp.com/mk8nxs37jXzmJf/img/rectangle-331-11.png",
+        image: brajesh
       },
       {
         name: "Dr. Rishi Mohan Bhatnagar",
         title: "MD",
         organization: "AA2IT India pvt Ltd",
-        image: "https://c.animaapp.com/mk8nxs37jXzmJf/img/rectangle-331-11.png",
+        image: rishimohan
       },
       {
         name: "Aman Gulati",
         title: "Deputy Director Head CoE - i4.0",
         organization: "NPC-DPIIT",
-        image: "https://c.animaapp.com/mk8nxs37jXzmJf/img/rectangle-331-11.png",
+        image: amangulati
       },
       {
         name: "Yadu Yadav",
         title: "Deputy Director",
         organization: "NPC-DPIIT CoE i4.0 , Udyami Bharat 4.0",
-        image: "https://c.animaapp.com/mk8nxs37jXzmJf/img/rectangle-331-11.png",
+        image: yaduyadav
       },
       {
         name: "Dhiraj Agrawal",
         title: "Chief Business OfficerMufin",
         organization: "Green Finance",
-        image: "https://c.animaapp.com/mk8nxs37jXzmJf/img/rectangle-331-11.png",
+        image: dhirajagg
       },
       {
         name: "Rajan Gupta",
         title: "Director Data Science and AI/ML",
         organization: "Deutsche Telekom Digital Labs",
-        image: "https://c.animaapp.com/mk8nxs37jXzmJf/img/rectangle-331-11.png",
+        image: rajangupta
       },
       {
         name: "Sandeep Rajoriya",
         title: "Assistant Director General of Foreign Trade",
         organization: "Ministry of Commerce and Industry,Government of India",
-        image: "https://c.animaapp.com/mk8nxs37jXzmJf/img/rectangle-331-11.png",
+        image: sandeep
       },
       {
         name: "Arpit Sharma",
         title: "CEO",
         organization: "Skill Council for Green Jobs",
-        image: "https://c.animaapp.com/mk8nxs37jXzmJf/img/rectangle-331-11.png",
+        image:arpitsharma
       },
   ];
